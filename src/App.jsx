eaae24cd885c7b0bc38a8b96d725c11e3af226ee
moin ud din zafar <moinseo037@gmail.com>
2025-07-27 18:44:04 +0500

    fix: remove nested router & use relative routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-import Homepage from './pages/homepage';
-import BlogPage from './subcomponents/BlogPage';
-import BlogList from './subcomponents/BlogList';
+import Homepage    from './pages/homepage';
+import BlogPage    from './subcomponents/BlogPage';
+import BlogList    from './subcomponents/BlogList';
 import ProjectList from './subcomponents/ProjectList';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/project-list" element={<ProjectList />} />
-        <Route path="/blog-list" element={<BlogList />} />
-        <Route path="/blog/:id" element={<BlogPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      {/* matches "/portfolio/" */}
+      <Route index element={<Homepage />} />
+
+      {/* matches "/portfolio/project-list" */}
+      <Route path="project-list" element={<ProjectList />} />
+
+      {/* matches "/portfolio/blog-list" */}
+      <Route path="blog-list" element={<BlogList />} />
+
+      {/* matches "/portfolio/blog/:id" */}
+      <Route path="blog/:id" element={<BlogPage />} />
+
+      {/* catch‑all: 404 under /portfolio/* */}
+      <Route path="*" element={<Homepage />} />
+    </Routes>
   );
 }
